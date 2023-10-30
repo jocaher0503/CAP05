752d814d6e0d495f0ced3922fc74d40be617c71f
jocaher0503 <jocaher0503@g.educaand.es>
2023-10-30 09:12:38 +0100

    30/10/2023

--- CAP05/src/CAP05EJ25.java
+++ CAP05/src/CAP05EJ25.java
@@ -1,14 +1,20 @@
 public class CAP05EJ25 {
+  public static int voltea (int valor){
+    int alreves=0;
+    boolean salida= false;
+    while (!salida) {
+      int digito=valor%10;
+      alreves=alreves*10 + digito;
+      if (valor<10)
+        salida=true;
+      else
+        valor=valor/10;
+    }
+    return alreves;
+  }
   public static void main(String[] args) {
     System.out.print("Introduzca un número entero: ");
-    int numeroIntroducido = Integer.parseInt(System.console().readLine());
-    int numero = numeroIntroducido;
-    int volteado = 0;
-    while (numero > 0) {
-      volteado = (volteado * 10) + (numero % 10);
-      numero /= 10;
-    } 
-    System.out.print("Si le damos la vuelta al " + numeroIntroducido);
-    System.out.println(" tenemos el " + volteado + ".");
+    int valor = Integer.parseInt(System.console().readLine());
+    System.out.print("Si le damos la vuelta al"+ valor +" tenemos el "+ voltea(valor));
   }
 }
