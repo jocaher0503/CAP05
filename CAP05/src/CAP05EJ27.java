527396f34046238b9b3e0f96332be93e83b6641d
jocaher0503 <jocaher0503@g.educaand.es>
2023-10-30 08:31:32 +0100

    30/10/2023

--- CAP05/src/CAP05EJ27.java
+++ CAP05/src/CAP05EJ27.java
@@ -1,5 +1,16 @@
 public class CAP05EJ27 {
     public static void main(String[] args) {
-        
-    }
-}
+        System.out.print("Introduzca un número entero que sea mayor que 1: ");
+        int numeroIntroducido=Integer.parseInt(System.console().readLine());
+        int contar=0;
+        int suma=0;
+        for (int i=1; i<numeroIntroducido; i++) {
+            if ((i % 3) == 0) {
+                System.out.print(i + " ");
+                contar++;
+                suma += i;
+              }
+            }
+            System.out.print("\nDesde 1 hasta " + numeroIntroducido + " hay " + contar + " múltiplos de 3 y entre ellos suman " + suma + ".");
+          }
+        }
