b600aef7578814c0fb4d2cb04d3a0600258356b7
jocaher0503 <jocaher0503@g.educaand.es>
2023-11-02 08:38:04 +0100

    2/11/2023

--- CAP05/src/CAP05EJ33.java
+++ CAP05/src/CAP05EJ33.java
@@ -1,5 +1,18 @@
 public class CAP05EJ33 {
     public static void main(String[] args) {
-        
+    System.out.print("Introduzca la altura de la U: ");
+    int altura = Integer.parseInt(System.console().readLine());
+    for (int i = 1; i < altura; i++) {
+        System.out.print("* ");
+    for (int j = 2; j < altura; j++) {
+        System.out.print("  ");
+    }
+    System.out.println("*");
+    }
+    System.out.print("  ");
+    for (int i = 2; i < altura; i++) {
+        System.out.print("* ");
+        }
     }
 }
+
