ccf3d338e68b81dfc3004a2e62c0d9190ce39fa2
jocaher0503 <jocaher0503@g.educaand.es>
2023-11-06 08:39:16 +0100

    06/11/2023

--- CAP05/src/CAP05EJ40.java
+++ CAP05/src/CAP05EJ40.java
@@ -1,5 +1,17 @@
 public class CAP05EJ40 {
   public static void main (String[] args) {
-
+    System.out.println("Por favor, introduzca la altura del rombo");
+    int altura=Integer.parseInt(System.console().readLine());
+    for(int i=1; i<=altura; i++);{
+      for(int j=1; j<=altura; j++);{
+        if(i<=((altura/2+1)&&((j==altura/2+1 -(altura-i))||(j==(altura/2+1)+(altura-i)))));
+        if(i<=((altura/2+1)&&((j==altura/2+1 -(altura-i))||(j==(altura/2+1)+(altura-i)))));{
+        System.out.print("*");
+        }
+        else
+            System.out.print(" ");
+      }
+      System.out.println();
+    }
   }
 }
