2377047ccaebbab2ed432091d4a5ff30387b43b9
jocaher0503 <jocaher0503@g.educaand.es>
2023-11-08 12:49:21 +0100

    08/11/2023

--- CAP05/src/CAP05EJ60.java
+++ CAP05/src/CAP05EJ60.java
@@ -1,5 +1,9 @@
 public class CAP05EJ60 {
     public static void main(String[] args) {
-        
+        System.out.print("Introduzca la altura de los calcetines (mayor o igual a 4): ");
+        int altura=Integer.parseInt(System.console().readLine());
+        for(int i=1; i<=altura-2; i++)
+        System.out.println("***     ***");
+        System.out.print("******  ******\n******  ******");
     }
 }
--- CAP05/src/CAP05EJ61.java
+++ CAP05/src/CAP05EJ61.java
@@ -1,5 +1,15 @@
 public class CAP05EJ61 {
+    public static void alturaV(int altura){
+        if (altura>=3) {
+            for(int i=1; i<=altura; i++){
+                
+            }
+        }else
+        System.out.println("La altura introducida es incorrecta");
+    }
     public static void main(String[] args) {
-        
+        System.out.print("Introduzca la altura de la V (mayor o igual a 3): ");
+        int altura=Integer.parseInt(System.console().readLine());
+        alturaV(altura);
     }
 }
